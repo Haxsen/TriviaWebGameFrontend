d8c56e2e0cebe466ef6977052c0ae59d4215b44b
Haxsen <hassaan.ali.lel@gmail.com>
2025-01-13 02:27:03 +0500

    Fix API_URL for react app (requirement)

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // we should use a config class for env vars but this is just a quick way
-const API_URL = process.env.API_URL || "https://localhost:7218/api/Trivia";
+const API_URL = process.env.REACT_APP_API_URL || "https://localhost:7218/api/Trivia";
 
 export const getTriviaQuestions = async () => {
     const response = await axios.get(`${API_URL}/questions`);
